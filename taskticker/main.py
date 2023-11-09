248d8e8b6929bd0961dabec568a686e09da8cd2a
ConsultaddEngineering <mandeep.l@consultadd.com>
2023-11-09 14:17:42 +0530

    posted to log1

--- taskticker/main.py
+++ taskticker/main.py
@@ -1,3 +1,5 @@
+from datetime import date
+
 from helper import (
     is_from_aws_event_bridge,
     is_slack_command,
@@ -11,7 +13,8 @@ from helper import (
     get_button_action_id,
     update_slack_message,
     get_submission,
-    get_update_modal
+    get_update_modal,
+    post_updates_to_log1, retrieve_update_details
 )
 from config import SLACK_CLIENT, DEFAULT_SNOOZE_DELAY
 from scheduler_worker import send_notifications, schedule_notification
@@ -31,10 +34,17 @@ def lambda_handler(event: dict, context):
         # If the event is a Slack Command
         if is_slack_command(body):
             print("Slack Command:", body)
-            SLACK_CLIENT.views_open(
-                trigger_id=body['trigger_id'][0],
-                view=get_setup_modal(body['channel_id'][0])
-            )
+            if body['command'][0] == '/setup':
+                SLACK_CLIENT.views_open(
+                    trigger_id=body['trigger_id'][0],
+                    view=get_setup_modal(body['channel_id'][0])
+                )
+            elif body['command'][0] == '/update':
+                res = SLACK_CLIENT.views_open(
+                    trigger_id=body['trigger_id'][0],
+                    view=get_update_modal(body['channel_id'][0])
+                )
+                print('saving update response', res)
             return {
                 "statusCode": 204,
             }
@@ -53,7 +63,15 @@ def lambda_handler(event: dict, context):
                     }
                 if submission == 'project_update_view':
                     print('project update view')
-                    
+                    update = retrieve_update_details(payload)
+                    res = post_updates_to_log1(
+                        project_id=1212,
+                        update=update['update'],
+                        sprint_start=date.today(),
+                        sprint_end=date.today()
+                    )
+                    print('log1 response', res)
+
             if is_button_pressed(payload):
                 action_id = get_button_action_id(payload)
                 if action_id == 'update_now_action':
@@ -70,7 +88,21 @@ def lambda_handler(event: dict, context):
                         channel=payload['channel']['id'],
                         post_at=int(payload['actions'][0]['action_ts'].split('.')[0]) + DEFAULT_SNOOZE_DELAY
                     )
-                    update_slack_message(payload['response_url'], {'text': 'Okay will remind you in an hour'})
+                    update_slack_message(
+                        response_url=payload['response_url'],
+                        message={
+                            "text": "Okay will remind you in an hour.",
+                            "blocks": [
+                                {
+                                    "type": "section",
+                                    "text": {
+                                        "type": "mrkdwn",
+                                        "text": "Okay will remind you in an hour.\n"
+                                                "Or you can post update anytime with `/update` command"
+                                    }
+                                }
+                            ]
+                        })
 
             return {
                 "statusCode": 204
